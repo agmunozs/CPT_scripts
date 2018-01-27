2cb57881276928abf786130003003716e96066f2
Angel G. Munoz <agmunoz@iri.columbia.edu>
2018-01-27 17:09:28 -0500

    Summary
    Additional setups added

--- fcst_prob_PCR_CPT_seasonal_precip.sh
+++ fcst_prob_PCR_CPT_seasonal_precip.sh
@@ -11,6 +11,7 @@
 # + Old data in the input folder is deleted at the beginning of the process!
 # + User needs to know how many EOFs to use for PCR.
 # + Rainfall observations are CPC Unified (Chen et al 2008).
+# + Cross-validation windows is set to be 3 yrs.
 
 ####START OF USER-MODIFIABLE SECTION##########################################################################
 
@@ -20,13 +21,14 @@ declare -a mon=('Jan')  #if just a particular month is desired
 #Target months
 declare -a tgti=('1.5')   #S: start for the DL
 declare -a tgtf=('3.5')   #S: end for the DL 
-declare -a tgt=('Feb-Apr')#for now, just write the period(s) corresponding a tgti to tgtf (for DL) for each init
-#Initial and final years (hindcasts)
-iyear=1982
-fyear=2010
-#Start year to forecast
+declare -a tgt=('Feb-Apr') #just write the target period(s) (for DL) for each init
+#Initial and end years (hindcasts) and length of training period
+iyear=1982  #typically 1982 for NMME models (must be [1982,2010])
+fyear=2018  #typically last year available (must be [2011,today])
+lent=30     #length of training period
+#Start year to forecast (don't change for now)
 fsyear=2018
-#Number of years to forecast
+#Number of years to forecast (don't change for now)
 nyear=1
 #Spatial domain for predictor
 nla1=29 # Nothernmost latitude
@@ -34,12 +36,12 @@ sla1=23 # Southernmost latitude
 wlo1=82 # Westernmost longitude
 elo1=90 # Easternmost longitude
 #Spatial domain for predictand
-nla2=29 # Nothernmost latitude
-sla2=23 # Southernmost latitude
-wlo2=82 # Westernmost longitude
-elo2=90 # Easternmost longitude
-#Number of EOFs for predictand:
-nEOFs=
+nla2=27 # Nothernmost latitude
+sla2=24 # Southernmost latitude
+wlo2=83 # Westernmost longitude
+elo2=88 # Easternmost longitude
+#Maximum number of EOFs for predictand: (minimum is always 1 in this script)
+nEOF=6
 #PATH to CPT root directory
 cptdir='/Users/agmunoz/Documents/Angel/CPT/CPT/15.7.3/'
 
@@ -69,22 +71,18 @@ export CPT_BIN_DIR=${cptdir}
 #Start loop 
 for mo in "${mon[@]}"
 do
-#Download hindcasts
-url='http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.CMC1-CanCM3/.HINDCAST/.MONTHLY/.prec/S/%280000%201%20'${mo}'%20'${iyear}'-'${fyear}'%29/VALUES/L/%28'${tgti}'%29/%28'${tgtf}'%29/RANGEEDGES/%5BL%5D//keepgrids/average/%5BM%5D/average/Y/%2823N%29/%2829N%29/RANGEEDGES/X/%2882E%29/%2890E%29/RANGEEDGES/-999/setmissing_value/%5BX/Y%5D%5BL/S/add%5D/cptv10.tsv'
+#Download hindcasts and forecasts
+url='http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.CMC1-CanCM3/.HINDCAST/.MONTHLY/.prec/S/%280000%201%20'${mo}'%20'${iyear}'-2010%29/VALUES/SOURCES/.Models/.NMME/.CMC1-CanCM3/.FORECAST/.MONTHLY/.prec/S/%280000%201%20'${mo}'%202011-'${fyear}'%29/VALUES/appendstream/L/%28'${tgti}'%29/%28'${tgtf}'%29/RANGEEDGES/%5BL%5D//keepgrids/average/%5BM%5D/average/Y/%28'${sla1}'%29/%28'${nla1}'%29/RANGEEDGES/X/%28'${wlo1}'%29/%28'${elo1}'%29/RANGEEDGES/-999/setmissing_value/%5BX/Y%5D%5BL/S/add%5D/cptv10.tsv'
 echo $url
 echo ---------------------------------------------------
-echo Downloading hindcasts and observations for $tgt initialized in $mo ...
-curl -k ''$url'' > hindcast_precip_${tgt}_ini${mon}.tsv
-
-#Download forecasts
-url='http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.CMC1-CanCM3/.FORECAST/.MONTHLY/.prec/S/%280000%201%20'${mo}'%20'${fsyear}'%29VALUES/L/%28'${tgti}'%29/%28'${tgtf}'%29RANGEEDGES%5BL%5D//keepgrids/average%5BM%5Daverage/Y/%2823N%29%2829N%29RANGEEDGES/X/%2882E%29%2890E%29RANGEEDGES%5BX/Y%5D%5BL/S/add%5D/cptv10.tsv'
-echo $url
-echo ---------------------------------------------------
-echo Downloading hindcasts and observations for $tgt initialized in $mo ...
-curl -k ''$url'' > forecast_precip_${tgt}_ini${mon}.tsv
+echo Downloading hindcasts and forecasts for $tgt initialized in $mo ...
+curl -k ''$url'' > model_precip_${tgt}_ini${mon}.tsv
 
 #Download observations
-url='http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.CPC-CMAP-URD/prate/T/%28Jan%20'${iyear}'%29/%28Dec%20'${fyear}'%29/RANGE/T/3/runningAverage/T/%28'${tgt}'%29/VALUES/Y/%2824N%29/%2827N%29/RANGEEDGES/X/%2883E%29/%2888E%29/RANGEEDGES/-999/setmissing_value/%5BX/Y%5D%5BT%5Dcptv10.tsv'
+url='http://iridl.ldeo.columbia.edu/SOURCES/.Models/.NMME/.CPC-CMAP-URD/prate/T/%28Jan%20'${iyear}'%29/%28Dec%20'${fyear}'%29/RANGE/T/3/runningAverage/T/%28'${tgt}'%29/VALUES/Y/%28'${sla2}'%29/%28'${nla2}'%29/RANGEEDGES/X/%28'${wlo2}'%29/%28'${elo2}'%29/RANGEEDGES/-999/setmissing_value/%5BX/Y%5D%5BT%5Dcptv10.tsv'
+echo $url
+echo ---------------------------------------------------
+echo Downloading observations for $tgt initialized in $mo ...
 curl -k ''$url'' > obs_precip_${tgt}.tsv
 
 
@@ -96,13 +94,15 @@ echo Producing CPT scripts for $tgt initialized in $mo ...
   
 cat  <<< '#!/bin/bash 
 '${cptdir}'CPT.x <<- END
-614 # Opens GCM validation
+612 # Opens PCR
 1 # Opens X input file
-../input/hindcast_precip_'${tgt}'_ini'${mon}'.tsv
+../input/model_precip_'${tgt}'_ini'${mon}'.tsv
 '${nla1}' # Nothernmost latitude
 '${sla1}' # Southernmost latitude
 '${wlo1}' # Westernmost longitude
 '${elo1}' # Easternmost longitude
+1 		  # Minimum number of modes
+'${nEOF}' # Maximum number of modes
 
 2 # Opens Y input file
 ../input/obs_precip_'${tgt}'.tsv
@@ -112,19 +112,24 @@ cat  <<< '#!/bin/bash
 '${elo2}' # Easternmost longitude
 
 4 # X training period
-1982 # First year of X training period
+'${iyear}' # First year of X training period
 5 # Y training period
-1982 # First year of Y training period
+'${iyear}' # First year of Y training period
+6 # Forecast period settings
+'${fsyear}' 
+9 # Number of forecasts
+'${nyear}'
 
 531 # Goodness index
 3 # Kendalls tau
 
 7 # Option: Lenght of training period
-29 # Lenght of training period 
+'${lent}' # Lenght of training period 
 8 # Option: Length of cross-validation window
 3 # Enter length
 
-#542 # Turn ON zero bound for Y data
+541 # Turn ON Transform predictand data
+542 # Turn ON zero bound for Y data
 545 # Turn ON synchronous predictors
 #561 # Turn ON p-values for skill maps
 
@@ -140,12 +145,15 @@ cat  <<< '#!/bin/bash
 1 # Number of near-neighours
 4 # Best near neighbour
 
+#554 # Transformation seetings
+#1   #Empirical distribution
+
 
 # Cross-validation
 112 # save goodness index
 ../output/PRCP_Kendallstau_raw_'${tgt}'_ini'${mon}'.txt
 
-#######BUILD MODEL  !!!!!
+#######BUILD MODEL AND VALIDATE IT  !!!!!
 311 # Cross-validation
 
 #131 # select output format
@@ -170,17 +178,26 @@ cat  <<< '#!/bin/bash
 11 # save 2AFC score
 ../output/PRCP_RocAbove_raw_'${tgt}'_ini'${mon}'.txt
 
-#0 # Stop saving  (not needed in newest version of CPT)
+#######FORECAST(S)  !!!!!
+455 # Probabilistic (3 categories) maps
+
+111 # Output results
+501 # Forecast probabilities
+../output/PRCP_PCRFCST_PROB_'${tgt}'_ini'${mon}'.txt
+#502 # Forecast odds
+0 #Exit submenu
+
+0 # Stop saving  (not needed in newest version of CPT)
 
 0 # Exit
 END
-' > RawSkill_precip_${tgt}_ini${mon}.cpt 
+' > PCRForecast_precip_${tgt}_ini${mon}.cpt 
 
 #Execute CPT and produce skill maps
 echo ---------------------------------------------------
 echo Executing CPT and producing skill maps for $mo ...
-chmod 755 RawSkill_precip_${tgt}_ini${mon}.cpt  
-./RawSkill_precip_${tgt}_ini${mon}.cpt 
+chmod 755 PCRForecast_precip_${tgt}_ini${mon}.cpt  
+./PCRForecast_precip_${tgt}_ini${mon}.cpt 
 
 cd ../input
 
